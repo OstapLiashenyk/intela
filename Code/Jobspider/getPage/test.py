7104f623d69496739521256721bd84718669ce63
oooly <ostap.liashenyk@gmail.com>
2024-02-13 23:00:24 +0200

    Spider page

--- Code/Jobspider/getPage/test.py
+++ Code/Jobspider/getPage/test.py
@@ -20,14 +20,13 @@ class Question:
 
     def __str__(self):
         return (f"ID: {self.id}\nPosted: {self.posted}\nJob Function Sought: {self.jobFunctionSought}\n"
-                f"Desired Industry: {self.desiredIndustry}\nLocation: {self.location}\n"
-                f"Objective: {self.objective}\nExperience: {self.experience}\n"
-                f"Education: {self.education}\nAffiliations: {self.affiliations}\n"
-                f"Skills: {self.skills}\nAdditional Information: {self.additionalInformation}\n"
-                f"Reference: {self.reference}")
+                f"\nDesired Industry: {self.desiredIndustry}\nLocation: {self.location}\n"
+                f"\nObjective: {self.objective}\nExperience: {self.experience}\n"
+                f"\nEducation: {self.education}\nAffiliations: {self.affiliations}\n"
+                f"\nSkills: {self.skills}\nAdditional Information: {self.additionalInformation}\n"
+                f"\nReference: {self.reference}")
 
 def getPageQuestions(url): # Returns an list of objects
-    questions = []  # Initialize list to collect Question objects
     try:
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
         response = requests.get(url, headers=headers)  # Assuming SSL verification is enabled
@@ -48,18 +47,21 @@ def getPageQuestions(url): # Returns an list of objects
             "Affiliations": "affiliations"
         }
 
+        question = Question()
+
         for content in desired_content:
             if content.name == 'font':
                 current_category = content.find('b').text.strip().replace(':', '')
                 if current_category in category_map:
-                    print(current_category)
-                    print(content.find('font'))
-        print(123)
+                    print(category_map[current_category])
+                    text_content = str(content.find('font')).replace('<br>', '\n').replace('<br/>', '\n')
+                    if hasattr(question, category_map[current_category]):
+                        setattr(question, category_map[current_category], text_content)
     except requests.exceptions.RequestException as e:
         print(f"Error during requests to {url} : {str(e)}")
 
-    return questions  # Return the list of Question objects
+    return question  # Return the list of Question objects
 
 # Example usage
 scraped_questions = getPageQuestions("https://www.jobspider.com/job/view-resume-39673.html")
-
+print(scraped_questions)
